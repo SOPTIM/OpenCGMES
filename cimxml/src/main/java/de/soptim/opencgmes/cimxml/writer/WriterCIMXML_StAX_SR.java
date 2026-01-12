14f0213ae59e9e652ba7e12ef5e16362671d1b41
Lukas Wilke <lukas.wilke@soptim.de>
2026-01-12 14:06:29 +0100

    Implementierung von WriterCIMXML_StAX_SR

---
+++ cimxml/src/main/java/de/soptim/opencgmes/cimxml/writer/WriterCIMXML_StAX_SR.java
@@ -0,0 +1,105 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.soptim.opencgmes.cimxml.writer;
+
+import de.soptim.opencgmes.cimxml.sparql.core.CimDatasetGraph;
+import java.io.OutputStream;
+import java.io.Writer;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import org.apache.jena.riot.RiotException;
+import org.apache.jena.riot.system.ErrorHandler;
+import org.apache.jena.riot.system.ErrorHandlerFactory;
+import org.apache.jena.riot.system.PrefixMap;
+import org.codehaus.stax2.XMLOutputFactory2;
+import org.codehaus.stax2.XMLStreamWriter2;
+
+/**
+ * CIMXML parser. This implementation is based on the RDF/XML reader ReaderRDFXML_StAX_SR in Apache
+ * Jena, originally. It has been adapted to the CIMXML needs.
+ * <p>
+ * This implementation uses StAX via {@link XMLStreamReader}.
+ *
+ * @see <a
+ * href="https://webstore.iec.ch/en/publication/25939">https://webstore.iec.ch/en/publication/25939</a>
+ */
+public class WriterCIMXML_StAX_SR {
+
+  public static XMLOutputFactory2 createXMLOutputFactory() {
+    var factory = new com.fasterxml.aalto.stax.OutputFactoryImpl();
+    factory.configureForSpeed();
+    return factory;
+  }
+
+  private static final XMLOutputFactory2 xmlOutputFactory = createXMLOutputFactory();
+
+  public static boolean TRACE = false;
+
+  public final ErrorHandler errorHandler;
+
+  public WriterCIMXML_StAX_SR() {
+    this(ErrorHandlerFactory.errorHandlerStd);
+  }
+
+  public WriterCIMXML_StAX_SR(ErrorHandler errorHandler) {
+    this.errorHandler = errorHandler;
+  }
+
+  public void write(OutputStream out, CimDatasetGraph cimDatasetGraph) {
+    write(out, cimDatasetGraph, null);
+  }
+
+  public void write(OutputStream out, CimDatasetGraph cimDatasetGraph, PrefixMap prefixMap) {
+    write(out, cimDatasetGraph, prefixMap, null);
+  }
+
+  public void write(OutputStream out, CimDatasetGraph cimDatasetGraph, PrefixMap prefixMap,
+      String baseURI) {
+    try {
+      var xmlStreamWriter = (XMLStreamWriter2) xmlOutputFactory.createXMLStreamWriter(out);
+      serialize(xmlStreamWriter, cimDatasetGraph, prefixMap, baseURI);
+    } catch (XMLStreamException ex) {
+      throw new RiotException("Failed to create the XMLStreamWriter", ex);
+    }
+  }
+
+  public void write(Writer out, CimDatasetGraph cimDatasetGraph) {
+    write(out, cimDatasetGraph, null);
+  }
+
+  public void write(Writer out, CimDatasetGraph cimDatasetGraph, PrefixMap prefixMap) {
+    write(out, cimDatasetGraph, prefixMap, null);
+  }
+
+  public void write(Writer out, CimDatasetGraph cimDatasetGraph, PrefixMap prefixMap,
+      String baseURI) {
+    try {
+      var xmlStreamWriter = (XMLStreamWriter2) xmlOutputFactory.createXMLStreamWriter(out);
+      serialize(xmlStreamWriter, cimDatasetGraph, prefixMap, baseURI);
+    } catch (XMLStreamException ex) {
+      throw new RiotException("Failed to create the XMLStreamWriter", ex);
+    }
+  }
+
+  private void serialize(XMLStreamWriter2 xmlStreamWriter, CimDatasetGraph cimDatasetGraph,
+      PrefixMap prefixMap, String baseURI) {
+    //TODO
+    // var serializer = new TBD
+  }
+}
