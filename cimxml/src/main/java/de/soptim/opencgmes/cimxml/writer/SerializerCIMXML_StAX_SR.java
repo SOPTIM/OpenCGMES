e2abb4e75a602a427122620c5255444f4d842a1d
Lukas Wilke <lukas.wilke@soptim.de>
2026-01-14 16:25:12 +0100

    Implementierung von Compound Properties

--- cimxml/src/main/java/de/soptim/opencgmes/cimxml/writer/SerializerCIMXML_StAX_SR.java
+++ cimxml/src/main/java/de/soptim/opencgmes/cimxml/writer/SerializerCIMXML_StAX_SR.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package de.soptim.opencgmes.cimxml.writer;
 
 import de.soptim.opencgmes.cimxml.sparql.core.CimDatasetGraph;
@@ -113,21 +131,30 @@ public class SerializerCIMXML_StAX_SR {
   }
 
   // Section 7.2.3.7 Compound element
-  private void writeCompoundElement() throws XMLStreamException {
-
+  private void writeCompoundElement(Resource subjectNode) throws XMLStreamException {
+    var typeResource = subjectNode.getProperty(RDF.type).getResource();
+    xmlStreamWriter.writeStartElement(typeResource.getNameSpace(), typeResource.getLocalName());
+    var propertyIterator = subjectNode.listProperties();
+    while (propertyIterator.hasNext()) {
+      writeProperty(propertyIterator.next());
+    }
+    xmlStreamWriter.writeEndElement();
   }
 
   // Section 7.2.3.8 Literal-Property element
   private void writeLiteralPropertyElement(Statement property) throws XMLStreamException {
     xmlStreamWriter.writeStartElement(property.getPredicate().getNameSpace(),
         property.getPredicate().getLocalName());
-    xmlStreamWriter.writeRaw(property.getLiteral().getValue().toString());
+    xmlStreamWriter.writeCharacters(property.getLiteral().getValue().toString());
     xmlStreamWriter.writeEndElement();
   }
 
   // Section 7.2.3.9 Compound-Property element
-  private void writeCompoundPropertyElement() throws XMLStreamException {
-
+  private void writeCompoundPropertyElement(Statement property) throws XMLStreamException {
+    xmlStreamWriter.writeStartElement(property.getPredicate().getNameSpace(),
+        property.getPredicate().getLocalName());
+    writeCompoundElement(property.getResource());
+    xmlStreamWriter.writeEndElement();
   }
 
   // Section 7.2.3.10 Resource-Property element
@@ -143,10 +170,12 @@ public class SerializerCIMXML_StAX_SR {
     if (property.getPredicate().equals(RDF.type)) {
       return;
     }
-    if (property.getObject().isLiteral()) {
-      writeLiteralPropertyElement(property);
-    } else {
+    if (property.getObject().isURIResource()) {
       writeResourcePropertyElement(property);
+    } else if (property.getObject().isLiteral()) {
+      writeLiteralPropertyElement(property);
+    } else { // isAnon
+      writeCompoundPropertyElement(property);
     }
   }
 
